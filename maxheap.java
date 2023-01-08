e470abca5c9051a529b6e7bdfb779d03f3a0b070
MaruthiSarathChandra <106025479+MaruthiSarathChandra@users.noreply.github.com>
2023-01-08 21:26:46 +0530

    Update maxheap.java

--- maxheap.java
+++ maxheap.java
@@ -1,6 +1,8 @@
 import java.util.*;
+
 public class maxhp {
     List<Integer> array = new ArrayList<Integer>();
+    //This Heap method is used to develop heap and insert values in to heap with out voilating the max-heap property
     public void Heap(Integer key) {
         array.add(key);
         int i = array.size()-1;
@@ -11,8 +13,10 @@ public class maxhp {
             i = i/2;
         }
     }
+    // This Heap_deletion method deletes root node and re-arrange the values in heap to maintain max-heap property.
     public void Heap_deletion(Integer Index) {
-        if(array.size()-1 == 0){
+        if (array.size()-1 == 0) {
+            System.out.println(array.get(0)+" is removed");
             array.remove(array.get(0));
         } else {
             int x = array.get(array.size()-1);
@@ -20,32 +24,35 @@ public class maxhp {
             System.out.println(array.get(0)+" is removed");
             array.set(0,x);
             int indexx = 0;
-            while((indexx*2)+1 <= array.size()-1 || ((indexx*2)+2 <= array.size()-1)) {
-                if ((indexx * 2) + 2 > array.size() - 1) {
+            while((indexx*2)+1 <= array.size()-1 || ((indexx*2)+2 <= array.size()-1)) { 
+                if ((indexx * 2) + 2 > array.size() - 1) { 
                     if (array.get((indexx * 2) + 1) > array.get(indexx)) {
                         x = array.get(indexx);
                         array.set(indexx, array.get((indexx * 2) + 1));
                         array.set((indexx * 2) + 1, x);
                         indexx = (indexx * 2) + 1;
                     } else {
+                        System.out.println(array);
                         return;
                     }
-                } else if (array.get((indexx * 2) + 1) > array.get((indexx * 2) + 2)) {
-                    if (array.get((indexx*2)+1) > array.get(indexx)){
+                } else if (array.get((indexx * 2) + 1) >= array.get((indexx * 2) + 2)) { 
+                    if (array.get((indexx*2)+1) > array.get(indexx)) {
                         x = array.get(indexx);
                         array.set(indexx, array.get((indexx * 2) + 1));
                         array.set((indexx * 2) + 1, x);
                         indexx = (indexx * 2) + 1;
                     } else {
+                        System.out.println(array);
                         return;
                     }
-                } else if (array.get((indexx * 2) + 1) < array.get((indexx * 2) + 2)) {
-                    if (array.get((indexx*2)+2) > array.get(indexx)){
+                } else if (array.get((indexx * 2) + 1) <= array.get((indexx * 2) + 2)) {
+                    if (array.get((indexx*2)+2) > array.get(indexx)) {
                         x = array.get(indexx);
                         array.set(indexx, array.get((indexx * 2) + 2));
                         array.set((indexx * 2) + 2, x);
                         indexx = (indexx * 2) + 2;
                     } else {
+                        System.out.println(array);
                         return;
                     }
                 }
@@ -56,10 +63,10 @@ public class maxhp {
     public static void main(String[] args) {
         maxhp obj = new maxhp();
         Integer[] arr = {5,2,4,1,6,8,1,0,10,11,3};
-        for(int j: arr) {
+        for(int j: arr){
             obj.Heap(j);
         }
-        System.out.println(obj.array);
+        System.out.println(obj.array+" "+obj.array.size());
         int y = obj.array.size();
         for(int i=0; i < y;i++) {
             obj.Heap_deletion(0);
