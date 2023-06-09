bccd359621da829246f60b34b79f233295e485fb
MaruthiSarathChandra <106025479+MaruthiSarathChandra@users.noreply.github.com>
2023-06-09 11:53:05 +0530

    dijkstraAlgorithm.py

--- dijkstraAlgorithm.py
+++ dijkstraAlgorithm.py
@@ -1,5 +1,5 @@
-
-matrix = {
+#testcase graph 1 
+adjList = {
     "A": {"B":2, "C": 4},
     "B": {"A": 2,"C": 1, "D": 7},
     "C": {"E": 3,"A":4,"B": 2},
@@ -8,28 +8,32 @@ matrix = {
     "F": {"D": 1,"E": 5}
 }
 
+# testcase graph 2 
+adjList_1 = {
+    "A": {"B": 50, "D": 10, "C": 45},
+    "B": {"D": 15, "C": 10},
+    "C": {"E": 30},
+    "D": {"A": 10, "E": 15},
+    "E": {"B": 20, "C": 35},
+    "F": {"E": 3}
+}
 
 #this dct dictonary is used to store shortest distance of each node
 dct = dict()
-#this visited dictonary is used to store whether the node is visited or not this will decrease the time complexity.without this it may lead to Exception maximum recursion
-visited = dict()
-for i in matrix.keys():
+for i in adjList_1.keys():
     dct[i] = float("inf")
-    visited[i] = False
-    
-    
-    
+
 def dijkstraAlgorithm(node):
-    if visited[node]:
-        return node
-    for i in matrix[node].items():
+    if node not in adjList_1.keys():
+        return 
+    for i in adjList_1[node].items():
         if dct[i[0]] > dct[node] + i[1]:
             dct[i[0]] = dct[node] + i[1]
-            visited[node] = True
-        dijkstraAlgorithm(i[0])
+            dijkstraAlgorithm(i[0])
 
 dct["A"] = 0
 dijkstraAlgorithm("A")
 
+print(dct["B"]) 
+#call the destination node u choose from dct dictonary which gives the shortest distance value from starting node to destination node after the exicution of dijkstraAlgorithm algorithm 
 
-print(dct["F"])
